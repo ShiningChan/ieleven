c0f31bbc890b0c7e9c10cc39ff4dacf84326016b
ShiningChan <chenweiqingme@126.com>
2018-09-04 19:07:11 +0800

    edit

--- test.py
+++ test.py
@@ -30,19 +30,52 @@ URL = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.f
 with request.urlopen(URL, timeout=4) as f:
     data = f.read()     #bytes型记得decode
     
+    
+'''
+#定义解析类     
+class WeatherSaxHandler(object):
+    def start_element(self, name, attrs):
+        print('sax:start_element: %s, attrs: %s' % (name, str(attrs)))
+        
+        
+    def end_element(self, name):
+        print('sax:end_element: %s' % name)
+        
+    def data_element(self, text):
+        print('sax:char_data: %s' % text)
+'''    
 
 #定义解析类     
 class WeatherSaxHandler(object):
     def start_element(self, name, attrs):
-        
-        
+        global weather_dict, which_day
+        # 判断并获取XML文档中地理位置信息
+        # sax:start_element: yweather:location, attrs: {'xmlns:yweather': 'http://xml.weather.yahoo.com/ns/rss/1.0', 'city': 'Beijing', 'country': 'China', 'region': ' Beijing'}
+        # sax:end_element: yweather:location
+        if name == 'yweather:forecast':
+            weather_dict['city'] = attrs['city']
         
     def end_element(self, name):
+        print('sax:end_element: %s' % name)
+        
     def data_element(self, text):
-    
+        print('sax:char_data: %s' % text)
+        
+        
+handler = WeatherSaxHandler()
 parser = ParserCreate()
+parser.StartElementHandler = handler.start_element
+parser.EndElementHandler = handler.end_element
+parser.CharacterDataHandler = handler.data_element
 
-    
+## parser.Parse(data.decode('utf-8'))
+
+weather_dict = {}
+which_day = 0
+
+
+
+'''
 def parseXml(xml_str):
     print(xml_str)
     return{
@@ -65,3 +98,4 @@ def parseXml(xml_str):
             }
         ]
     }
+    '''
