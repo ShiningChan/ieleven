a93119d886d7231d526ddbb96884f7646f829e03
ShiningChan <chenweiqingme@126.com>
2018-09-17 23:02:51 +0800

    add psutil

--- test.py
+++ test.py
@@ -29,6 +29,8 @@ from PIL import Image, ImageFilter, ImageDraw, ImageFont
 import requests
 import chardet
 
+
+
 ## 获取CPU信息
 # CPU逻辑数量
 psutil.cpu_count()
@@ -59,6 +61,7 @@ for x in range(10):
 [10.8, 1.6, 3.1, 3.1]
 '''
 
+
 ## 获取内存信息
 # 获取物理内存
 psutil.virtual_memory()
@@ -68,6 +71,7 @@ psutil.virtual_memory()
 # 交换区内存信息
 psutil.swap_memory()
 #sswap(total=1073741824, used=150732800, free=923009024, percent=14.0, sin=10705981440, sout=40353792)
+
 
 ## 获取磁盘信息
 psutil.disk_partitions() # 磁盘分区信息
@@ -80,3 +84,58 @@ sdiskusage(total=998982549504, used=390880133120, free=607840272384, percent=39.
 psutil.disk_io_counters() # 磁盘IO
 sdiskio(read_count=988513, write_count=274457, read_bytes=14856830464, write_bytes=17509420032, read_time=2228966, write_time=1618405)
 
+
+## 获取网络接口和网络连接信息
+psutil.net_io_counters() # 获取网络读写字节／包的个数
+#snetio(bytes_sent=3885744870, bytes_recv=10357676702, packets_sent=10613069, packets_recv=10423357, errin=0, errout=0, dropin=0, dropout=0)
+
+psutil.net_if_addrs() # 获取网络接口信息
+{
+  'lo0': [snic(family=<AddressFamily.AF_INET: 2>, address='127.0.0.1', netmask='255.0.0.0'), ...],
+  'en1': [snic(family=<AddressFamily.AF_INET: 2>, address='10.0.1.80', netmask='255.255.255.0'), ...],
+  'en0': [...],
+  'en2': [...],
+  'bridge0': [...]
+}
+
+psutil.net_if_stats() # 获取网络接口状态
+{
+  'lo0': snicstats(isup=True, duplex=<NicDuplex.NIC_DUPLEX_UNKNOWN: 0>, speed=0, mtu=16384),
+  'en0': snicstats(isup=True, duplex=<NicDuplex.NIC_DUPLEX_UNKNOWN: 0>, speed=0, mtu=1500),
+  'en1': snicstats(...),
+  'en2': snicstats(...),
+  'bridge0': snicstats(...)
+}
+
+
+$ sudo python3      # 获取系统权限
+
+psutil.net_connections()    # 获取当前网络连接信息
+[
+    sconn(fd=83, family=<AddressFamily.AF_INET6: 30>, type=1, laddr=addr(ip='::127.0.0.1', port=62911), raddr=addr(ip='::127.0.0.1', port=3306), status='ESTABLISHED', pid=3725),
+    sconn(fd=84, family=<AddressFamily.AF_INET6: 30>, type=1, laddr=addr(ip='::127.0.0.1', port=62905), raddr=addr(ip='::127.0.0.1', port=3306), status='ESTABLISHED', pid=3725),
+    sconn(fd=93, family=<AddressFamily.AF_INET6: 30>, type=1, laddr=addr(ip='::', port=8080), raddr=(), status='LISTEN', pid=3725),
+    sconn(fd=103, family=<AddressFamily.AF_INET6: 30>, type=1, laddr=addr(ip='::127.0.0.1', port=62918), raddr=addr(ip='::127.0.0.1', port=3306), status='ESTABLISHED', pid=3725),
+    sconn(fd=105, family=<AddressFamily.AF_INET6: 30>, type=1, ..., pid=3725),
+    sconn(fd=106, family=<AddressFamily.AF_INET6: 30>, type=1, ..., pid=3725),
+    sconn(fd=107, family=<AddressFamily.AF_INET6: 30>, type=1, ..., pid=3725),
+    ...
+    sconn(fd=27, family=<AddressFamily.AF_INET: 2>, type=2, ..., pid=1)
+]
+
+
+
+## 寻找python所在进程
+for i in list_pid:
+    try:
+        m = psutil.Process(i)
+            if re.match('.ython*', m.name()) is not None:
+                print(i)
+    finally:
+        print('Not found')
+
+Not found
+14276
+Not found
+Not found
+Not found
